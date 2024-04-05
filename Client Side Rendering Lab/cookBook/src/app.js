49753f67427a50a0ff220afa7084801f20b5a6c7
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2024-04-05 10:46:04 +0300

    done with the first task

---
+++ Client Side Rendering Lab/cookBook/src/app.js
@@ -0,0 +1,68 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js';
+const main = document.querySelector("main");
+
+
+function loadRecipies() {
+    const url = 'http://localhost:3030/data/recipes';
+    fetch(url)
+        .then(data => data.json())
+        .then(data => {
+            const renderValue = Object.values(data).reverse().map(recipe => html`
+                <article @click=${getRecipeDescription} class="preview">
+                    <div class="title">
+                        <p style="display: none;">${recipe._id}</p>
+                        <h2>${recipe.name}</h2>
+                    </div>
+                    <div class="small">
+                        <img src="${recipe.img}">
+                    </div>
+                </article>
+            `)
+            render(renderValue, main)
+        })
+        .finally(() => {
+            document.querySelector('#loader').style.display = 'none';
+        });
+}
+
+loadRecipies();
+
+
+function getRecipeDescription(e) {
+    let userId = localStorage.getItem("userId");
+    userId = userId ? userId : '';
+
+    const url = `http://localhost:3030/data/recipes/${e.target.querySelector('p').textContent}`
+    fetch(url)
+        .then(data => data.json())
+        .then(data => {
+            const renderData =
+                html`
+    <article>
+        <h2>${data.name}</h2>
+        <div class="band">
+            <div class="thumb">
+                <img src="${data.img}">
+            </div>
+            <div class="ingredients">
+                <h3>Ingredients:</h3>
+                <ul>
+                    ${data.ingredients.map(i => html`<li>${i}</li>`)}
+                </ul>
+            </div>
+        </div>
+            <div class="description">
+                <h3>Preparation:</h3>
+                ${data.steps.map(s => html`<p>${s}</p>`)}
+            </div>
+            ${data._ownerId === userId ? html`<div style="width: 100%; text-align: center; padding: 10px;">
+            <p style="display: none;">${data._id}</p> 
+            <button onclick="editRecipe(event)" id="edit-recipe" style="border-radius: 5px; margin: 5px; padding: 5px 10px;">&#9998; Edit</button>
+            <button onclick="deleteRecipe(event)" id="delete-recipe" style="border-radius: 5px; margin: 5px; padding: 5px 10px;">&#10006; Delete</button>
+        </div>`:
+        ""}
+    </article>`;
+
+            render(renderData, main);
+        });
+}
